28f1a721b85cce7bb45eacd2abb57dfbc6daa3b8
txw123123 <linux_txw@sina.com>
2021-04-08 10:55:40 +0800

    last commit

---
+++ src/app.vue
@@ -0,0 +1,26 @@
+  <!-- app.vue -->
+
+  <template>
+    <div>
+	  <p>我是{{ user.name }}</p>
+	  <p>我来自{{ user.from }}</p>
+    </div>
+  </template>
+
+  <script>
+    export default {
+      data () {
+        return {}
+      },
+      props: {
+        user: {
+          type: Object
+        }
+      }
+    }
+  </script>
+
+  <style scoped lang='less'>
+  </style>
+
+
---
+++ src/index.js
@@ -0,0 +1,3 @@
+// index.js
+import NpmVue from './app.vue'
+export default NpmVue
